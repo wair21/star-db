19313cadd8686703ac6ce86f41360293f0b434ea
wair21 <wair21@gmail.com>
2020-04-05 10:40:10 +0300

    start project, add restapi

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class SwapiService {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url) {
+        const rec = await fetch(`${this._apiBase}${url}`);
+
+        // Если не все ок, то кидаем ошибку.
+        if(!rec.ok) {
+            throw new Error(`Could not fetch ${url}, received status ${rec.status}`);
+        }
+
+        return await rec.json();
+    }
+
+    // Получение всех людей
+    async getAllPeople() {
+      const rec = await this.getResource(`/people/`);
+      return rec.results;
+    }
+
+    // Получение информации по человеку по его id
+    getPerson(id) {
+        return this.getResource(`/people/${id}`);
+    }
+
+    // Получение всех планет
+    async getAllPlanets() {
+      const rec = await this.getResource(`/planets/`);
+      return rec.results;
+    }
+
+    // Получение информации по планете по его id
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}`);
+    }
+    // Получение всех кораблей
+    async getAllStarships() {
+      const rec = await this.getResource(`/starships/`);
+      return rec.results;
+    }
+
+    // Получение информации по кораблю по его id
+    getStarship(id) {
+        return this.getResource(`/starships/${id}`);
+    }
+}
+
+const swapi = new SwapiService();
+swapi.getAllPeople()
+    .then((rec) => {
+      console.log(rec);
+    });
+
+
+
+
